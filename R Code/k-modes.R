cc7ae4caafe667d8bfe626c4d9b34af2c5df682d
dedenistiawan <dedenistiawan@gmail.com>
2022-01-20 11:36:28 +0700

    initial commit

--- R Code/k-modes.R
+++ R Code/k-modes.R
@@ -3,9 +3,9 @@ library(klaR)
 library (readr)
 urlfile = "https://raw.githubusercontent.com/dedenistiawan/Dataset/main/Dataset%20DAS.csv"
 
-data<-read_csv(url(urlfile))
+Data<-read_csv(url(urlfile))
 
-cluster.results <-kmodes(data , 3, iter.max = 10, weighted = FALSE )
+cluster.results <-kmodes(Data , 3, iter.max = 10, weighted = FALSE )
 
 cluster.output <- cbind(data ,cluster.results$cluster)
 write.csv(cluster.output, file = "kmodes clusters.csv", row.names = TRUE)
